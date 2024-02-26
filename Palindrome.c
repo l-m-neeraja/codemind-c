5d2728e191ea0d7fa3ad905a5a4e570982aeba11
l-m-neeraja <151540886+l-m-neeraja@users.noreply.github.com>
2024-02-26 20:16:38 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int rev=0;     for(int i=n;i>0;i/=10){         int x=i%10;         rev=rev*10+x;     }     if(rev==n){         printf("Palindrome");     }     else{         printf("Not Palindrome");     } }

---
+++ Palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int rev=0;
+    for(int i=n;i>0;i/=10){
+        int x=i%10;
+        rev=rev*10+x;
+    }
+    if(rev==n){
+        printf("Palindrome");
+    }
+    else{
+        printf("Not Palindrome");
+    }
+}    
